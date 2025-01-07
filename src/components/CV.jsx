1ef0cae28180ce22fe634f0bd1e6b8e1b6107890
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-01-07 11:45:58 -0500

    wip: styling

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,22 +1,39 @@
 import '../styles/cv.css'
 
 
+
+
 function CV({formData}) {
+    const formatDate = (formDate) => {
+        return new Date(formDate).toLocaleString('default', {timeZone: "UTC", month:'long', year:'numeric'})
+    }
+
+
     return <div className='cv'>
         <div className='cv-general'>
             <h1>{formData['Name']}</h1>
+            <hr></hr>
+            <br></br>
             <p>{formData['Email']}</p>
             <p>{formData['Phone']}</p>
         </div>
+        <br></br>
         <div className='cv-education'>
+            <h1>Education</h1>
+            <hr></hr>
+            <br></br>
             <h2>{formData['School Name']}</h2>
-            <p>{formData['educationStart']} - {formData['educationEnd']}</p>
+            <p>{formatDate(formData['educationStart'])} - {formatDate(formData['educationEnd'])}</p>
             <p>{formData['Field of Study']}</p>
         </div>
+        <br></br>
         <div className='cv-experience'>
+            <h1>Experience</h1>
+            <hr></hr>
+            <br></br>
             <h2>{formData['Employer']}</h2>
             <h3>{formData['Position']}</h3>
-            <p>{formData['jobStart']} - {formData['jobEnd']}</p>
+            <p>{formatDate(formData['jobStart'])} - {formatDate(formData['jobEnd'])}</p>
             <p>{formData['jobDescription']}</p>
         </div>
 
